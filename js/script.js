beaa2c0d37d308c1db4c092179ff944fead3d2e8
AndriiMuravskyi <AndriiMuravskyi@users.noreply.github.com>
2020-10-29 16:08:16 +0200

    222

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 "use strict"
 
-let browser = prompt('kakoy u tebya brauzer?')
+const number = +prompt('Введите число между 0 и 3');
 
-if (browser == 'Edge') {
-    alert ('ne poveslo!');
-} else if (browser == 'Crome') {
-    alert('poveslo');
-} else if (browser == 'zzz') {
-    console.log('yyyyy')
+switch (number) {
+    case 0:
+        alert ('Вы ввели число 0');
+    break;
+    case 1:
+        alert ('Вы ввели число 1');
+    break;
+    case 2:
+    case 3:
+        alert ('Вы ввели число 2, а может и 3');
+    break;
+
+    default:
+        alert ('moya ne ponimat')
 }
-    
